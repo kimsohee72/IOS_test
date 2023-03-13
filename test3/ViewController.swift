fcb3bdb742f6a69bf4496b56630b6d5e805adf21
kimsohee72 <cowhappy721@naver.com>
2023-03-14 01:03:08 +0900

    test3

--- test3/ViewController.swift
+++ test3/ViewController.swift
@@ -1,15 +1,9 @@
-//
-//  ViewController.swift
-//  TestIOS
-//
-//  Created by 김소희 on 2023/03/13.
-//
-
 import UIKit
 
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     let MAX_ARRAY_NUM = 10
     let PICKER_VIEW_COLUMN = 1
+    let PICKER_VIEW_HEIGHT:CGFloat = 80
     var imageFileName = ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg","10.jpg"]
     
     var imageArray = [UIImage?]()
@@ -34,9 +28,18 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return imageFileName.count
     }
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return imageFileName[row]
-    }
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return PICKER_VIEW_HEIGHT
+    } // -> 이미지를 띄우는 공간의 크기 키우기
+//    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+//        return imageFileName[row]
+//    } // -> 글자를 이용하여 이미지를 선택하는 PickerView 만들기
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let imageView = UIImageView(image: imageArray[row])
+        imageView.frame=CGRect(x: 0, y: 0, width: 100, height: 150)
+        
+        return imageView
+    } // -> 이미지를 이용하여 이미지를 선택하는 PickerView 만들기
     func pickerView(_ pickerView: UIPickerView, didSelectRow row:Int, inComponent component: Int) {
         lblImageFileName.text = imageFileName[row]
         imageView.image = imageArray[row]
