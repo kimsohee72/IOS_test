40207c71a7a82e790d36b2b1df842c08cb61d73f
kimsohee72 <cowhappy721@naver.com>
2023-03-13 15:59:53 +0900

    test1

---
+++ test1/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  TestIOS
+//
+//  Created by 김소희 on 2023/03/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var btnResize: UIButton!
+    
+    var isZoom = false
+    var imgOn: UIImage?
+    var imgOff: UIImage?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        imgOn = UIImage(named: "lamp_on.png")
+        imgOff = UIImage(named: "lamp_off.png")
+        
+        imgView.image = imgOn
+    }
+
+    @IBAction func btnResizImage(_ sender: UIButton) {
+        let scale: CGFloat = 2.0
+        var newWidth : CGFloat,newHeight: CGFloat
+        if (isZoom){
+            newWidth = imgView.frame.width/scale
+            newHeight = imgView.frame.height/scale
+            btnResize.setTitle("확대", for: .normal)
+        }
+        else{
+            newWidth = imgView.frame.width*scale
+            newHeight = imgView.frame.height*scale
+            btnResize.setTitle("축소", for: .normal)
+        }
+        imgView.frame.size = CGSize(width: newWidth, height: newHeight)
+        isZoom = !isZoom
+    }
+    
+    @IBAction func switchImageOnOff(_ sender: UISwitch) {
+        if sender.isOn{
+            imgView.image = imgOn
+        }
+        else{
+            imgView.image = imgOff
+        }
+    }
+}
+
